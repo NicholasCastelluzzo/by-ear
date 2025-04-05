fac3ca4285d60a963ad2603acf0dd838587d5d81
Matthew Whalen <whalenm20@uky.edu>
2025-04-05 02:13:57 -0400

    Feature: Detecting basic tempo

--- detect_pitch.py
+++ detect_pitch.py
@@ -4,6 +4,44 @@ import matplotlib.pyplot as plt
 import csv
 import sys
 
+def get_octave(note: str):
+    for char in note:
+        if char.isnumeric():
+            return int(char)
+        
+def get_pitch(note: str):
+    return note[:-1]
+
+def remove_higher_octaves(notes):
+    lowest_octaves = {}
+
+    for note in notes:
+        pitch = get_pitch(note)
+        octave = get_octave(note)
+        if pitch not in lowest_octaves:
+            lowest_octaves[pitch] = octave
+        elif pitch in lowest_octaves and octave < lowest_octaves[pitch]:
+            lowest_octaves[pitch] = octave
+
+    print(lowest_octaves)
+    for i in range(len(notes)-1, -1, -1):
+        note = notes[i]
+        pitch = get_pitch(note)
+        octave = get_octave(note)
+        #print(note, pitch, octave)
+        if lowest_octaves[pitch] != octave:
+            notes.pop(i)
+    
+    return notes
+
+def calc_avg_energy(data):
+    n = len(data)
+    data = np.power(data, 2)
+    data = np.sum(data)
+    energy = data / n
+    print(energy*100)
+    return energy*100
+
 def create_pitch_dict() -> dict:
     pitch_data = csv.DictReader(open("pitch_chart.csv"))
     pitch_dict = {}
@@ -29,12 +67,10 @@ def get_note_from_freq(freq: float, pitch_dict: dict) -> str:
 
     return None
 
-def find_freq_from_wav(wav_file: str) -> list:
-    data, samplerate = sf.read(wav_file)
-
-    mono_data = data[:,0]
-
-    fft = np.fft.fft(mono_data)
+def find_freqs(data, samplerate) -> list:
+    if calc_avg_energy(data) < 0.01:
+        return []
+    fft = np.fft.fft(data)
     fft = np.power(fft, 2)
     lp_fft = 10 * np.log10(fft)
 
@@ -42,11 +78,11 @@ def find_freq_from_wav(wav_file: str) -> list:
     real_lp_fft = [max(val, 0) for val in real_lp_fft]
     
 
-    pot_freqs = np.fft.fftfreq(len(mono_data), 1/samplerate)
+    pot_freqs = np.fft.fftfreq(len(data), 1/samplerate)
     # plt.plot(pot_freqs[:len(pot_freqs)//2+1], real_lp_fft)
     # plt.show()
     pitch_freqs = []
-    threshold = 0.9 * np.max(real_lp_fft)
+    threshold = 0.99 * np.max(real_lp_fft)
     for i in range(len(real_lp_fft)):
         mag = real_lp_fft[i]
         if mag >= threshold:
@@ -55,11 +91,12 @@ def find_freq_from_wav(wav_file: str) -> list:
     
     return pitch_freqs
 
-def detect_pitches_from_wav(wav_file: str) -> list:
-    freqs = find_freq_from_wav(wav_file)
+def detect_pitches(data, samplerate) -> list:
+    freqs = find_freqs(data, samplerate)
     pitch_dict = create_pitch_dict()
     notes = list({get_note_from_freq(freq, pitch_dict) for freq in freqs})
+    notes = remove_higher_octaves(notes)
     return notes
 
-notes = detect_pitches_from_wav("music/CMajor_Test.wav")
-print(notes) 
+# notes = detect_pitches_from_wav("music/Mary_Had_A_Little_Lamb.mp3")
+# print(notes) 
